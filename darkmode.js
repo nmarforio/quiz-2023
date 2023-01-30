206d86b9892e6d9046607f7f542d6c4272f96340
nmarforio <n.marforio@gmail.com>
2023-01-30 13:55:35 +0100

    darkmode changes

--- darkmode.js
+++ darkmode.js
@@ -1,17 +1,22 @@
 const dark = document.querySelector('[data-js="darkmode"]');
 const bodyElement = document.querySelector('[data-js="body"]');
-const quizElement = document.querySelector('[data-js="quiz]');
-const footerElement = document.querySelector('[data-js="pages"]');
-const cardElement = document.querySelector('[data-js="cards"]');
+const quizElement = document.querySelector('[data-js="quiz"]');
 const settingButton = document.querySelector('[data-js="setting"]');
+const darkImagine = document.querySelector('[data-js="imagine"]');
 
 dark.addEventListener("click", () => {
   bodyElement.classList.toggle("dark");
-  quizElement.classList.toggle("dark");
-  footerElement.classList.toggle("dark");
-  cardElement.classList.toggle("dark");
-  settingButton.classList.toggle("dark");
   dark.classList.toggle("dark");
+  settingButton.classList.toggle("dark");
+  quizElement.classList.toggle("dark");
+  darkImagine.classList.toggle("dark");
+
+  if (darkImagine.src === "https://pic.onlinewebfonts.com/svg/img_87237.png") {
+    darkImagine.src =
+      "https://clipground.com/images/white-profile-icon-clipart-3.jpg";
+  } else {
+    darkImagine.src = "https://pic.onlinewebfonts.com/svg/img_87237.png";
+  }
 
   if (dark.textContent === "🌚") {
     dark.textContent = "🌝";
